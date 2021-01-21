d6e6e3e3230a0d28c28926123eec30b9a81070a7
Akrouch <guijakrouch@gmail.com>
2021-01-21 11:09:00 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
-import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-    <GlobalStyle />
+    <h1>BEM VINDO PARSA</h1>
+    <h2>Este projeto contém:</h2>
+    <h3>Types:</h3>
+    <ul>
+      <li>jest-dom: ^5.11.4</li>
+      <li>react: ^11.1.0</li>
+      <li>user-event: ^12.1.10</li>
+      <li>jest: ^26.0.15</li>
+      <li>node: ^12.0.0</li>
+      <li>react: ^16.9.53</li>
+      <li>react-dom: ^16.9.8</li>
+      <li>uuid: ^8.3.0</li>
+      <li>yup: ^0.29.9</li>
+      <li>react-router-dom: ^5.1.5</li>
+      <li>styled-components: ^5.1.0</li>
+    </ul>
+    <h3>Packages:</h3>
+    <ul>
+      <li>core: ^2.1.3</li>
+      <li>web: ^2.1.3</li>
+      <li>axios: ^0.21.0</li>
+      <li>date-fns: ^2.16.1</li>
+      <li>polished: ^4.0.5</li>
+      <li>react: ^17.0.1</li>
+      <li>react-dom: ^17.0.1</li>
+      <li>react-icons: ^3.11.0</li>
+      <li>react-router-dom: ^5.2.0</li>
+      <li>react-scripts: 4.0.1</li>
+      <li>react-spinners: ^0.9.0</li>
+      <li>react-spring: ^8.0.27</li>
+      <li>styled-components: ^5.1.0</li>
+      <li>typescript: 4.0.5</li>
+      <li>uuid: ^8.3.1</li>
+      <li>web-vitals: ^0.2.4</li>
+      <li>yup: ^0.29.3</li>
+    </ul>
   </>
 );
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 
-import githubBackground from '../assets/github.svg';
-
 export default createGlobalStyle`
  * {
    margin: 0;
@@ -11,19 +9,15 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F0f0f5 url(${githubBackground}) no-repeat 70% top;
-    -webkit-font-smoothing: antialiased
+    -webkit-font-smoothing: antialiased;
+    margin: 0 auto;
   }
 
   body, input, button {
     font: 16px Roboto, sans-serif
   }
 
-  #root {
-    max-width: 960px;
-    margin: 0 auto;
-    padding: 40px 20px;
-  }
+
 
   button {
     cursor: pointer
